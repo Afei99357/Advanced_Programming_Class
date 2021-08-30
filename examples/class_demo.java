d04f78565819338c7c7f1c3e9fe310008b82b1d4
Eric Liao <ericliaoyf@gmail.com>
2021-08-30 14:09:59 -0400

    new change

---
+++ examples/class_demo.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class class_demo {
+    public static void main(String[] args){
+        int x = 4;
+        if (x ==4)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
