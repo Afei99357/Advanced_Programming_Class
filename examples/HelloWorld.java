027445774925980bca3dae3fc98f030217f3818f
Eric Liao <ericliaoyf@gmail.com>
2021-08-30 13:39:15 -0400

    new change

--- examples/HelloWorld.java
+++ examples/HelloWorld.java
@@ -3,5 +3,6 @@ package examples;
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Please close me!");
     }
 }
