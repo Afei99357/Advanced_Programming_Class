b6e54de4a2a1d8a3f6b3b2242a9903527877c4d1
Yunfei Liao <42871795+Afei99357@users.noreply.github.com>
2021-08-30 13:50:17 -0400

    Update HelloWorld.java
    conflict

--- examples/HelloWorld.java
+++ examples/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Please close me!");
+        System.out.println("nothing")
     }
 }
