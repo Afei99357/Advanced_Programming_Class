f45032293b00d19e61a0709b4b35ee6b565fda94
Eric Liao <ericliaoyf@gmail.com>
2021-09-10 11:22:01 -0400

    adding new function for amino acid test

--- examples/AminoAcidTest.java
+++ examples/AminoAcidTest.java
@@ -1,6 +1,8 @@
 package examples;
 
 
+import java.sql.SQLOutput;
+
 public class AminoAcidTest
 {
     public static String[] SHORT_NAMES = { "A","R", "N", "D", "C", "Q", "E", "G",  "H", "I", "L", "K", "M", "F", "P",
@@ -11,10 +13,11 @@ public class AminoAcidTest
 
     public static void main(String[] args) throws InterruptedException
     {
-        System.out.println("Quiz will start in 5 seconds!");
-        Thread.sleep(5000);
+        System.out.println("Get Ready! The quiz will start in 3 seconds!");
+        Thread.sleep(3000);
         long startTime = System.currentTimeMillis();
 
+        int score = 0;
         for (int m=0; 1==1 ; m++){
             int i = (int) (Math.random() * 20);
             long currentTime = System.currentTimeMillis();
@@ -25,19 +28,21 @@ public class AminoAcidTest
                 if (aString.length() != 0){
                     String aChar = "" + aString.charAt(0);
                     if (aChar.equals(SHORT_NAMES[i])){
+                        score++;
                         continue;
-                    }
-                    else{
-                        System.out.println("The quiz has been stopped due to a wrong answer!");
+                    }else{
+                        long currentTimeFinish = System.currentTimeMillis();
+                        System.out.println("\nThe quiz is finished due to a wrong answer!");
+                        System.out.println("Your score for this test is: " + score +
+                                "， Time cost: " + (currentTimeFinish - startTime)/ 1000 + " seconds.");
                         break;
                     }
-                }
-                else{
+                }else{
                     System.out.println("Please insert a validate letter!");
                 }
-            }
-            else{
-                System.out.println("The time is out!");
+            }else{
+                System.out.println("\nThe time is out!");
+                System.out.println("Your score for this test is: " + score);
                 break;
             }
         }
