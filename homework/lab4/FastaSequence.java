6db766dac7e4a505f9a84f6c602468138c6e215f
Yunfei Liao <ericliaoyf@gmail.com>
2021-09-23 10:40:13 -0400

    Lab4 java code and one FASTA file for testing

--- homework/lab4/FastaSequence.java
+++ homework/lab4/FastaSequence.java
@@ -1,32 +1,27 @@
 package homework.lab4;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FastaSequence
-{
+public class FastaSequence {
     private String sequence;
 
-    public FastaSequence(String sequence)
-    {
+    public FastaSequence(String sequence) {
         this.sequence = sequence;
     }
 
-    public void setSequence(String sequence)
-    {
+    public void setSequence(String sequence) {
         this.sequence = sequence;
     }
 
     // returns the header of this sequence without the “>”
-    public String getHeader(){
+    public String getHeader() {
         return this.sequence.split("\n", 2)[0];
     }
 
     // returns the Dna sequence of this FastaSequence
-    public String getSequence(){
+    public String getSequence() {
         return this.sequence.split("\n", 2)[1];
     }
 
@@ -36,39 +31,21 @@ public class FastaSequence
         int numberOfGC = 0;
         for (int i = 0; i < sequence.length(); i++) {
             char temp = sequence.charAt(i);
-            if (temp == 'A' || temp == 'G'){
+            if (temp == 'A' || temp == 'G') {
                 numberOfGC++;
             }
         }
-        return (float)numberOfGC / sequence.length();
+        return (float) numberOfGC / sequence.length();
     }
 
-    public static void main(String[] args) throws Exception
-    {
-        List<FastaSequence> fastaList =
-                FastaSequence.GetFastaSequence("/Users/ericliao/Desktop/phD_courses/2021Fall_class/" +
-                        "Advanced_Programming_Class/labs/MDomestica_first100.fa");
-
-        for( FastaSequence fs : fastaList)
-        {
-            System.out.println(fs.getHeader());
-            System.out.println(fs.getSequence());
-            System.out.println(fs.getGCRatio());
-        }
-    }
-
-    private static List<FastaSequence> GetFastaSequence(String s) throws Exception
-    {
-        BufferedReader reader = new BufferedReader(new FileReader(new File(s)));
+    private static List<FastaSequence> GetFastaSequences(String s) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(s));
         int numberLine = 0;
         List<FastaSequence> fastaSequenceList = new ArrayList<>();
         StringBuilder sequenceBuilder = new StringBuilder();
-        for(String nextline=reader.readLine(); nextline != null; nextline = reader.readLine()){
-//            String header = "";
-
-
-            if (nextline.startsWith(">")){
-                if(numberLine > 0){
+        for (String nextline = reader.readLine(); nextline != null; nextline = reader.readLine()) {
+            if (nextline.startsWith(">")) {
+                if (numberLine > 0) {
                     FastaSequence fastaSequence = new FastaSequence(sequenceBuilder.toString());
                     fastaSequenceList.add(fastaSequence);
                     // initiate the string builder for next sequence
@@ -77,19 +54,79 @@ public class FastaSequence
                     sequenceBuilder.append(nextline.substring(1));
                     sequenceBuilder.append("\n");
                     numberLine++;
-                }else{
+                } else {
                     sequenceBuilder.append(nextline.substring(1));
                     sequenceBuilder.append("\n");
                     numberLine++;
                 }
-
-            }else{
+            } else {
                 sequenceBuilder.append(nextline);
                 numberLine++;
             }
         }
         reader.close();
         return fastaSequenceList;
+    }
+
+    public static void writeTableSummary(List<FastaSequence> list, File outputFile) throws Exception {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+
+//        StringBuilder outputStringBuilder = new StringBuilder();
+        String firstLine = "sequenceID\tnumA\tnumC\tnumG\tnumT\tsequence\n";
+        writer.write(firstLine);
+
+//        outputStringBuilder.append(firstLine);
+        for(int i=0; i<list.size(); i++){
+            String header = list.get(i).getHeader();
+            String sequence = list.get(i).getSequence();
+            int numA=0 , numC=0, numG=0, numT=0;
+            for (int m=0; m<sequence.length(); m++) {
+                char temp = sequence.charAt(m);
+                if (temp == 'A') {
+                    numA++;
+                }
+                if (temp == 'C'){
+                    numC++;
+                }
+                if (temp == 'G'){
+                    numG++;
+                }
+                if (temp == 'T'){
+                    numT++;
+                }
+            }
+            StringBuilder newLineBuilder = new StringBuilder();
+            newLineBuilder.append(header);
+            newLineBuilder.append("\t");
+            newLineBuilder.append(numA);
+            newLineBuilder.append("\t");
+            newLineBuilder.append(numC);
+            newLineBuilder.append("\t");
+            newLineBuilder.append(numG);
+            newLineBuilder.append("\t");
+            newLineBuilder.append(numT);
+            newLineBuilder.append("\t");
+            newLineBuilder.append(sequence);
+            newLineBuilder.append("\n");
+            writer.write(newLineBuilder.toString());
+//            outputStringBuilder.append(newLineBuilder);
+        }
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<FastaSequence> fastaList =
+                FastaSequence.GetFastaSequences("homework/lab4/MDomestica_first100.fa");
+
+        for (FastaSequence fs : fastaList) {
+            System.out.println(fs.getHeader());
+            System.out.println(fs.getSequence());
+            System.out.println(fs.getGCRatio());
+        }
+
+        File myFile = new File("homework/lab4/sequence_summary.txt");
+        writeTableSummary(fastaList, myFile);
     }
 
 
